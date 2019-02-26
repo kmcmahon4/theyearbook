eed0f33c5a108e12217e739608212fc31a9fe121
kmcmahon4 <kmcmahon4@student.sjcny.edu>
2019-02-25 19:50:45 -0500

    homework 2.25.19

--- app/src/main/java/com/example/myyearbook/MainActivity.java
+++ app/src/main/java/com/example/myyearbook/MainActivity.java
@@ -2,12 +2,32 @@ package com.example.myyearbook;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+
+
+        Button btn = (Button) findViewById(R.id.linkButton);
+        btn.setOnClickListener(new OnClickListener() {
+
+
+            public void onClick(View v) {
+                Intent myWebLink = new Intent(android.content.Intent.ACTION_VIEW);
+                myWebLink.setData(Uri.parse("https://www.cnn.com/2019/02/25/perspectives/samsung-galaxy-foldable-phone/index.html"));
+                startActivity(myWebLink);
+            }
+
+        });
+}
 }
